49ea9e0f0d84b32c551b6989d3ed08aa6ae99051
4am <a2_4am@sent.com>
2021-06-11 23:36:53 -0400

    suite verified passing on Passport 2021-06-07 (some binary changes due to more efficient nibtable patcher)

--- run-all.sh
+++ run-all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/Documents/a2/mame0229/mame apple2ee -autoboot_script passport.lua -flop1 ~/Documents/a2/passport/build/passport.dsk
+~/Documents/a2/mamesrc/mame apple2ee -nothrottle -frameskip 10 -sound none -autoboot_script passport.lua -flop1 ~/Documents/a2/passport/build/passport.dsk
