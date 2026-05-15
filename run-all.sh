6acfb33376025cc66f8ed55a1d90d03289b6179b
4am <a2_4am@sent.com>
2026-05-15 11:00:48 -0400

    up to date and verified with Passport 2026-05-12

--- run-all.sh
+++ run-all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/Documents/a2/mamesrc/mame apple2ee -nothrottle -video none -sound none -autoboot_script passport.lua -flop1 ~/Documents/a2/passport/build/passport.dsk
+mame apple2ee -nothrottle -video none -sound none -autoboot_script passport.lua -flop1 ~/Documents/a2/passport/build/passport.dsk
